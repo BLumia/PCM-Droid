8618dbdd0ba54d0ba6b9161fed8e52e037c3e206
Gary Wang <wzc782970009@gmail.com>
2017-10-29 23:41:14 +0800

    server options popup menu

--- app/src/main/java/net/blumia/pcm/privatecloudmusic/MainActivity.java
+++ app/src/main/java/net/blumia/pcm/privatecloudmusic/MainActivity.java
@@ -1,17 +1,19 @@
 package net.blumia.pcm.privatecloudmusic;
 
-import android.app.Application;
 import android.content.Intent;
-import android.database.Cursor;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button btnOpenDrawer;
+    Button btnServerPopupMenu;
     DrawerLayout drawerSrvAndFolderList;
     ListView lvSongList;
     ListView lvServerIconList;
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         btnOpenDrawer = (Button) findViewById(R.id.btn_openDrawer);
+        btnServerPopupMenu = (Button) findViewById(R.id.btn_serverPopupMenu);
         drawerSrvAndFolderList = (DrawerLayout) findViewById(R.id.drawer_srvAndFolderList);
         lvServerIconList = (ListView) findViewById(R.id.lv_server_icon_list);
         lvSongList = (ListView) findViewById(R.id.lv_song_list);
@@ -51,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         infoList.add(new PCMServerInfo(2, "asd", null, null, null));
 
         btnOpenDrawer.setOnClickListener(this);
+        btnServerPopupMenu.setOnClickListener(this);
     }
 
     @Override
@@ -59,6 +64,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_openDrawer:
                 drawerSrvAndFolderList.openDrawer(Gravity.LEFT);
                 break;
+            case R.id.btn_serverPopupMenu:
+                PopupMenu popup = new PopupMenu(this, v);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.server_options, popup.getMenu());
+                popup.setOnMenuItemClickListener(new ServerOptionsPopupMenu());
+                popup.show();
+                break;
+        }
+    }
+
+    class ServerOptionsPopupMenu implements PopupMenu.OnMenuItemClickListener {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch  (item.getItemId()) {
+                case R.id.mi_update_server_info:
+                    Toast.makeText(getApplicationContext(), "R.id.mi_update_server_info", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.mi_delete_server:
+                    Toast.makeText(getApplicationContext(), "R.id.mi_delete_server", Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
         }
     }
 }
