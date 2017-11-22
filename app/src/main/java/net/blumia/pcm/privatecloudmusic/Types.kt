1266e2dbd18a5aeb2e323ed8b0bcccf99f73b24c
Gary Wang <wzc782970009@gmail.com>
2017-11-22 21:17:07 +0800

    types, permissions

---
+++ app/src/main/java/net/blumia/pcm/privatecloudmusic/Types.kt
@@ -0,0 +1,41 @@
+package net.blumia.pcm.privatecloudmusic
+
+import java.net.URL
+
+/**
+ * Created by wzc78 on 2017/11/22.
+ */
+enum class ServerType {
+    SRV_PCM, ADD_SRV
+}
+
+enum class PlaylistType {
+    FOLDER, PLAYLIST
+}
+
+enum class MusicItemType {
+    MUSIC, SUB_FOLDER
+}
+
+data class ServerItem (
+        var serverName : String,
+        var apiUrl : URL,
+        var fileRootUrl : URL,
+        var password : String,
+        var type : ServerType = ServerType.SRV_PCM
+)
+
+data class PlaylistItem (
+        var name : String,
+        var folderPath : String, // should be "xxx/xxx/", leave blank if type != folder
+        var type : PlaylistType = PlaylistType.FOLDER
+)
+
+data class MusicItem (
+        var name : String,
+        var filePathAndName : String, // only a filename is ok if using relative path
+        var modifyTime : Long,
+        var fileSize : Long,
+        var useRelativePath : Boolean = true,
+        var type : MusicItemType
+)
