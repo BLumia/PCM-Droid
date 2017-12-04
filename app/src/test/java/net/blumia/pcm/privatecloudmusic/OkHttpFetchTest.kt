518d508e16e466b60b3fa78d14e274243ca3a136
Gary Wang <wzc782970009@gmail.com>
2017-12-04 18:07:48 +0800

    unit test update

--- app/src/test/java/net/blumia/pcm/privatecloudmusic/OkHttpFetchTest.kt
+++ app/src/test/java/net/blumia/pcm/privatecloudmusic/OkHttpFetchTest.kt
@@ -1,11 +1,13 @@
 package net.blumia.pcm.privatecloudmusic
 
-import android.util.Log
 import okhttp3.*
+import okhttp3.mockwebserver.MockWebServer
 import org.junit.Test
 
 import org.junit.Assert.*
 import java.io.IOException
+import okhttp3.mockwebserver.MockResponse
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -15,14 +17,26 @@ import java.io.IOException
 class OkHttpFetchTest {
     @Test
     fun okHttpFetch_isCorrect() {
+        var server = MockWebServer()
+        server.enqueue(MockResponse().setBody("[Artist/Kevin%20Penkin]"))
+        server.start()
+
+        val baseUrl = server.url("/api.php")
+
         val httpClient = OkHttpClient()
         val formBody = FormBody.Builder()
                 .add("do", "getfilelist")
                 .build()
         val request = Request.Builder()
-                .url("https://pcm.blumia.cn/api.php")
+                .url(baseUrl)
                 .post(formBody)
                 .build()
+
+        val response = httpClient.newCall(request).execute()
+        val result = response!!.body()!!.string()
+        //System.out.println("Get:" + result)
+        assertEquals("[Artist/Kevin%20Penkin]", result)
+        /*
         httpClient.newCall(request).enqueue(object: okhttp3.Callback {
             override fun onFailure(call: Call?, e: IOException?) {
                 e?.printStackTrace()
@@ -30,9 +44,39 @@ class OkHttpFetchTest {
 
             override fun onResponse(call: Call?, response: Response?) {
                 val result = response!!.body()!!.string()
-                Log.e("result", result)
+                System.out.println(result)
+            }
+        })*/
+    }
+
+    @Test
+    fun realFetch_isCorrect() {
+        val httpClient = OkHttpClient()
+        val formBody = FormBody.Builder()
+                .add("do", "getfilelist")
+                .add("folder", "GameOST/")
+                .build()
+        val request = Request.Builder()
+                .url("https://pcm.blumia.cn/api.php")
+                .post(formBody)
+                .build()
+
+        /*
+        httpClient.newCall(request).enqueue(object: okhttp3.Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                e?.printStackTrace()
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val result = response!!.body()!!.string()
+                System.out.println(result)
             }
         })
-        assertEquals(4, 2 + 2)
+        Thread.sleep(2000)
+        */
+        val response = httpClient.newCall(request).execute()
+        val result = response!!.body()!!.string()
+        System.out.println("Get:" + result)
+
     }
 }
